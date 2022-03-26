29020df7b466ed4eb5f4e9a5099ed30fc310fa3f
Anirudh <purohitanirudh632@gmail.com>
2022-03-26 23:07:04 +0530

    update

---
+++ notes1.java
@@ -0,0 +1,9 @@
+public class notes1 {
+    public static void main(String[] args) {
+        System.out.println("here we are going to do some addition    :");
+        int a= 25;
+        int  b = 32;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
