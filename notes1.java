ff556472f2eceb101a85515ee260b4084aea5fcf
Anirudh <purohitanirudh632@gmail.com>
2022-03-27 07:38:31 +0530

    update

--- notes1.java
+++ notes1.java
@@ -1,9 +1,25 @@
-public class notes1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+class notes1 {
+ public static void main(String[] args) {
         System.out.println("here we are going to do some addition    :");
         int a= 25;
         int  b = 32;
         int c = a+b;
         System.out.println(c);
-    }
+        boolean isadult ;
+     System.out.println("enter your age please");
+     Scanner myobj = new Scanner(System.in);
+     int age;
+     age = myobj.nextInt();
+
+     if (age >= 18)
+     {
+         isadult =true;
+     }
+     else
+         isadult=false;
+
+     System.out.println("can this person vote : " + isadult);
+ }
 }
